8dbbb5b5e02e07cffcf1df2eb8da317cb2059233
Sharif <sharifnasser@gmail.com>
2020-11-03 23:30:28 -0600

    Add more routes

--- index.js
+++ index.js
@@ -13,7 +13,23 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Home is running!')
+    res.send('Home is running!');
+});
+
+app.get('/bio', (req, res) => {
+    res.send('Bio is running!');
+});
+
+app.get('/education', (req, res) => {
+    res.send('Education is running!');
+});
+
+app.get('/experience', (req, res) => {
+    res.send('Experience is running!');
+});
+
+app.get('/speaking', (req, res) => {
+    res.send('Speaking is running!');
 });
 
 // Database connection
