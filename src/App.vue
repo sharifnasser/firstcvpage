59ec373482f6b88001e74d2cfadb6f79ee6e8dbe
Sharif <sharifnasser@gmail.com>
2020-11-04 00:54:44 -0600

    Add homepage

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
-    </main>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Add font family -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900">
+    <title>Sharif Nasser</title>
+    
+    <div class="container">
+      <div class="row">
+        <aside class="col-md-5">
+            <h3 class="mb-1 text-muted">first CV page of</h3>
+            <h1 class="mb-1">
+                <a href="#">Sharif Nasser</a>
+            </h1>
+            <h3 class="mb-5 font-weight-light">
+                Robotics Student<br>
+                Microsof Learn Student Ambassador<br>
+                GitKraken Ambassador
+            </h3>
+            <h4 class="mb-0 font-weight-normal">
+                <a href="mailto:sharifnasser@gmail.com">sharifnasser@gmail.com</a>
+            </h4>
+            <p class="mb-0">
+                <a class="text-dark" href="https://www.linkedin.com/in/sharifnasser"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                <a class="text-dark" href="https://github.com/sharifnasser"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a class="text-dark" href="https://www.instagram.com/sharif_nasser"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a class="text-dark" href="https://twitter.com/sharif_nasser"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </p>
+        </aside>
+        
+        <main class="col-md-5">
+            <p>
+                <a href="./assets/sharif.jpg">
+                    <img src="./assets/sharif.jpg" alt="Sharif Nasser" class="rounded-sm" style="width: 100%">
+                </a>
+            </p>
+        </main>
+      </div>    
+    </div>
   </div>
 </template>
 
@@ -16,39 +51,25 @@ export default {
 }
 </script>
 
-<style>
-body {
-  margin: 0;
-}
+<style scoped>
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
 }
 
-main {
-  text-align: center;
-  margin-top: 40px;
+/* Style all font awesome icons */
+.fa {
+    padding: 20px;
+    font-size: 40px;
+    width: 60px;
+    text-align: center;
+    text-decoration: none;
 }
 
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
+/* Add a hover effect if you want */
+.fa:hover {
+    opacity: 1;
 }
 
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
 </style>
