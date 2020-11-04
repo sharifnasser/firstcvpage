ccd28357a3d71658a5b5dc1b8e059c94a52ea253
Sharif <sharifnasser@gmail.com>
2020-11-03 23:05:26 -0600

    Add database connection

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const mysql = require('mysql');
 const morgan = require('morgan');
+
 const app = express();
+
 // Settings
 app.set('port', process.env.PORT || 3000);
+
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+
+// Database connection
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'firstcvpage'
+});
+
+connection.connect( error => {
+    if (error) throw error;
+    console.log("Database connected");
+});
+
 // Server is listening
 app.listen(app.get('port'), () => {
     console.log(`Server is listening on port ${app.get('port')}`);
